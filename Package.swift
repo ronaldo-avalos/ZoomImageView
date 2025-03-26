ad11683f44da0ce47c7971c7e5f04d3f453ec5f6
Artem Horovenko <amolon210@icloud.com>
2025-03-26 23:25:50 +0000

    iOS version requirement increased to v15
    For native async-image usage

--- Package.swift
+++ Package.swift
@@ -6,22 +6,24 @@ import PackageDescription
 let package = Package(
     name: "ZoomImageView",
     platforms: [
-          .iOS(.v13) // Puedes ajustar la versión mínima según tus necesidades
-      ],
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZoomImageView",
             targets: ["ZoomImageView"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ZoomImageView"),
+            name: "ZoomImageView",
+            resources: [
+                .process("Resources")
+            ]
+        ),
         .testTarget(
             name: "ZoomImageViewTests",
             dependencies: ["ZoomImageView"]
         ),
     ]
 )
+
